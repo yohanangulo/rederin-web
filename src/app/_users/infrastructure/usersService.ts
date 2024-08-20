847c5032597fc7b3b3925ff3e14f40594e5b9186
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2024-08-20 16:38:42 -0400

    Console log removed

--- src/app/_users/infrastructure/usersService.ts
+++ src/app/_users/infrastructure/usersService.ts
@@ -5,8 +5,6 @@ import axios from 'axios'
 export async function listUsers() {
   const { data: users } = await axios.get<User[]>('https://rederin-api.vercel.app/api/users')
 
-  console.log(users.at(-1))
-
   return users
 }
 
